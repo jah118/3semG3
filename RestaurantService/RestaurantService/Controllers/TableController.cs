10e5d37978434dddbe31fc0a06085edfe5e71f92
Spandaueren <spandaueren@gmail.com>
2020-11-05 09:05:05 +0100

    Fixed csproj, initial get for TableController
    It didn't have a reference I gave it one

---
+++ RestaurantService/RestaurantService/Controllers/TableController.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess;
+using DataAccess.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestaurantService.Controllers
+{
+    public class TableController : ApiController
+    {
+        private readonly IRepository<RestaurantTablesDTO> _tableRepository;
+
+        public TableController(IRepository<RestaurantTablesDTO> tableRepository)
+        {
+            _tableRepository = tableRepository;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(_tableRepository.GetAll());
+        }
+    }
+}
