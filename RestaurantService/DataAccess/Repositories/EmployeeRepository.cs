f3b140658cfb9ff92f6dce3256b8f2255fa91fee
jah118 <>
2020-11-05 09:46:15 +0100

    Create EmployeeRepository.cs
    a copy off customer

---
+++ RestaurantService/DataAccess/Repositories/EmployeeRepository.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.DataTransferObjects;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccess.Repositories
+{
+    public class EmployeeRepository : IRepository<EmployeeDTO>
+    {
+        private readonly RestaurantContext _context;
+
+        public EmployeeRepository(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public CustomerDTO Create(EmployeeDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(EmployeeDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EmployeeDTO> GetAll()
+        {
+            var employees = _context.Employee.ToList();
+            var res = new List<EmployeeDTO>();
+            foreach (Employee e in employees)
+            {
+                res.Add(new EmployeeDTO
+                {
+                    FirstName = e.Id.ToString()
+                });
+            }
+
+            return res;
+        }
+
+        public EmployeeDTO GetById(int id)
+        {
+            return new EmployeeDTO { FirstName = "Lars", LastName = "Nysom", Address = "vej vej 8", City = "Ållern", Email = "la@nysom.dk", Phone = "22222222", ZipCode = "9000" };
+        }
+
+        public IEnumerable<EmployeeDTO> GetCountWithOffsetByOrdering(int count, int offset, string ordering)
+        {
+            List<Employee> res = null;
+
+            throw new NotImplementedException();
+        }
+
+        public EmployeeDTO Update(EmployeeDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
