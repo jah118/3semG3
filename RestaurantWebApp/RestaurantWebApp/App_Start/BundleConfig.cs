275d8408d05465cb8da62230c10c85272444bbe9
jah118 <jonasah97@gmail.com>
2020-11-19 20:46:13 +0100

    Reservation time added
    d

--- RestaurantWebApp/RestaurantWebApp/App_Start/BundleConfig.cs
+++ RestaurantWebApp/RestaurantWebApp/App_Start/BundleConfig.cs
@@ -13,6 +13,10 @@ namespace RestaurantWebApp
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+          
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                             "~/Content/bootstrap.css"));
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
