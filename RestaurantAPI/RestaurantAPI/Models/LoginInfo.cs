769dd24bfba69c75594fa9cc9a21faea00426fc1
Your Name <spandaueren@gmail.com>
2020-11-26 11:08:33 +0100

    Fixed broken associations, still not feature complete

---
+++ RestaurantAPI/RestaurantAPI/Models/LoginInfo.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.DataTransferObjects;
+
+namespace RestaurantAPI.Models
+{
+    public record LoginInfo
+    {
+        public UserDTO User { get; init; }
+        public string Username { get; init; }
+        public string Password { get; init; }
+        public UserRoles Role { get; init; }
+    }
+}
