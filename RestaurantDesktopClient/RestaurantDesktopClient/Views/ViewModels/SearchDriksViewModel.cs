4a7fec8087c9925c1a03980c0f071bd3104010ec
jah118 <jonasah97@gmail.com>
2020-11-30 15:55:04 +0100

    Revert "Revert "Desktopopdatering""
    This reverts commit 53a07891e3c6ca90cb54c389cccf42ddf9fbbafd.

--- RestaurantDesktopClient/RestaurantDesktopClient/Views/ViewModels/SearchDriksViewModel.cs
+++ RestaurantDesktopClient/RestaurantDesktopClient/Views/ViewModels/SearchDriksViewModel.cs
@@ -9,48 +9,99 @@ using System.Threading.Tasks;
 
 namespace RestaurantDesktopClient.Views.ViewModels
 {
-    class SearchDriksViewModel
+    public class ItemHandler
+    {
+        public ItemHandler()
+        {
+            Items = new List<FoodDTO>();
+        }
+
+        public List<FoodDTO> Items { get; private set; }
+
+        public void Add(FoodDTO item)
+        {
+            Items.Add(item);
+        }
+    }
+    public class SearchDriksViewModel
     {
         private DataTable _foodTable;
+        private ItemHandler _itemHandler;
         private readonly IRepository<FoodDTO> repository;
 
         public SearchDriksViewModel()
         {
             repository = new FoodRepository();
+            
         }
+        public event PropertyChangedEventHandler PropertyChanged;
 
         private void CreateSearchTable()
         {
-            _foodTable = new DataTable();
-            _foodTable.Columns.Add("Name", typeof(String));
-            _foodTable.Columns.Add("Price", typeof(String));
-            _foodTable.Columns.Add("Description", typeof(String));
+            _itemHandler = new ItemHandler();
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        public DataTable SearchTable
+        public List<FoodDTO> SearchTable
         {
             get
             {
                 IEnumerable<FoodDTO> foods = repository.GetAll();
                 if (foods != null)
                 {
-                    if (_foodTable == null) CreateSearchTable();
+                    if (_itemHandler == null) CreateSearchTable();
 
                     var sorted = from FoodDTO in foods.AsEnumerable() where FoodDTO.foodCategoryName.Equals("Drikkevare") select FoodDTO;
                     sorted.ToList<FoodDTO>().ForEach(x =>
                     {
-                        DataRow dr = _foodTable.NewRow();
-                        _foodTable.Rows.Add(dr.ItemArray = new[] { x.Name, x.Price.ToString("#.##"), x.Description});
+                        List<FoodDTO> bl = _itemHandler.Items;
+                        bl.Add(x);
+                    //    DataRow dr = _foodTable.NewRow();
+                    //    _foodTable.Rows.Add(dr.ItemArray = new[] { x.Name, x.Price.ToString("#.##"), x.Description });
                     });
                 }
-                return _foodTable;
+                return _itemHandler;
             }
             set
             {
-                _foodTable = value;
+                _itemHandler = value;
+
                 PropertyChanged(this, new PropertyChangedEventArgs("_foodTable"));
             }
         }
+    }
+
+
+        //    private void CreateSearchTable()
+        //    {
+        //        _foodTable = new DataTable();
+        //        _foodTable.Columns.Add("Name", typeof(String));
+        //        _foodTable.Columns.Add("Price", typeof(String));
+        //        _foodTable.Columns.Add("Description", typeof(String));
+        //    }
+
+        //    public DataTable SearchTable
+        //    {
+        //        get
+        //        {
+        //            IEnumerable<FoodDTO> foods = repository.GetAll();
+        //            if (foods != null)
+        //            {
+        //                if (_foodTable == null) CreateSearchTable();
+
+        //                var sorted = from FoodDTO in foods.AsEnumerable() where FoodDTO.foodCategoryName.Equals("Drikkevare") select FoodDTO;
+        //                sorted.ToList<FoodDTO>().ForEach(x =>
+        //                {
+        //                    DataRow dr = _foodTable.NewRow();
+        //                    _foodTable.Rows.Add(dr.ItemArray = new[] { x.Name, x.Price.ToString("#.##"), x.Description});
+        //                });
+        //            }
+        //            return _foodTable;
+        //        }
+        //        set
+        //        {
+        //            _foodTable = value;
+        //            PropertyChanged(this, new PropertyChangedEventArgs("_foodTable"));
+        //        }
+        //    }
     }
 }
