9e595a302978a3234948ffc615cbc38ef088bf87
jah118 <jonasah97@gmail.com>
2020-12-02 21:16:33 +0100

    temp this need to removed if no solution is found

--- RestaurantWebApp/RestaurantWebApp/Model/FoodViewModel.cs
+++ RestaurantWebApp/RestaurantWebApp/Model/FoodViewModel.cs
@@ -13,5 +13,8 @@ namespace RestaurantWebApp.Model
         public IEnumerable<DataTransferObject.FoodDTO> Drinks { get; set; }
         public DataTransferObject.FoodDTO SelectedDrink { get; set; }
 
+        public IEnumerable<DataTransferObject.FoodDTO> OrderFoodAndDrinks { get; set; }
+        public DataTransferObject.FoodDTO SelectedOrderFoodAndDrinks { get; set; }
+
     }
 }
