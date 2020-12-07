5d298ae9b885a7fac1e76f6b147ffc342a554a79
jah118 <jonasah97@gmail.com>
2020-12-07 09:40:39 +0100

    add validation

---
+++ RestaurantAPI/DataAccess/Models/ModelExtensions/ReservationExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using DataAccess.DataTransferObjects;
+
+namespace DataAccess.Models
+{
+    public partial class Reservation
+    {
+        public static bool Validate(ReservationDTO toValidate)
+        {
+            if (toValidate == null) return false;
+            if (Customer.Validate(toValidate.Customer)) return false;
+            if (toValidate.ReservationTime > DateTime.Today) return false;
+            if (0<(toValidate.NoOfPeople)) return false;
+            if (toValidate.Tables.ToList().Count > 0) return false;
+            //TODO VALIDATION
+            return true;
+        }
+    }
+}
