2a2b7d6b5fd4fe7ced523801c4d35de9486da4f7
jah118 <jonasah97@gmail.com>
2020-12-10 14:10:21 +0100

    Create ConvertStringToFoodLists.cs

---
+++ RestaurantWebApp/RestaurantWebApp/Util/ConvertStringToFoodLists.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantWebApp.DataTransferObject;
+using System;
+using System.Collections.Generic;
+
+namespace RestaurantWebApp.Util
+{
+    public static class ConvertStringToFoodLists
+    {
+        public static IList<FoodDTO> ListOfFoodsIdStringsToFoodList(List<string> stringList,
+            List<FoodDTO> foodsListFromApi)
+        {
+            var foods = new List<FoodDTO>();
+            foreach (var item in stringList)
+                if (int.TryParse(item, out var tempId))
+                {
+                    var food = foodsListFromApi.Find(x => x.Id == tempId);
+                    foods.Add(food);
+                }
+                else
+                {
+                    throw new FormatException("Fail to add one item to list");
+                }
+
+            return foods;
+        }
+    }
+}
